475fe16043f5f7b14278e77e579e58e70638d5d6
ABangoura <albangoura@aol.com>
2024-02-03 02:11:27 -0500

    Test1

---
+++ IJPDS/Exercise14_05/ColorAndFont.java
@@ -0,0 +1,10 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ColorAndFont extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}
