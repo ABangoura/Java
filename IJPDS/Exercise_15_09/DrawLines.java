31ab2554b27a667fc2f5b0d3cc9d4982cadf227f
ABangoura <albangoura@aol.com>
2024-02-29 22:22:19 -0500

    Finish exercise 15_09 and 15_10.

---
+++ IJPDS/Exercise_15_09/DrawLines.java
@@ -0,0 +1,82 @@
+/**
+ * Book: Introduction to Java Programming and Data Structures, 11th Edition (Y. Daniel Liang). 
+ * Exercise 15_09.
+ * (Draw lines using the arrow keys) Write a program that draws line segments
+ * using the arrow keys. The line starts from (100, 100) in the pane and draws
+ * toward east, north, west, or south when the right-arrow key, up-arrow key, leftarrow key, or down-arrow key is pressed, as shown in Figure 15.28b.
+ * 
+ * @author Almamy Bangoura
+ * @version 2/27/24
+ */
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Line;
+import javafx.stage.Stage;
+
+
+public class DrawLines extends Application {
+    // Starting point.
+    private double startX = 100;
+    private double startY = 100;
+    private double endX = 100;
+    private double endY = 100;
+    
+    @Override
+    public void start(Stage primaryStage) {
+        // Create a Pane for the drawing area.
+        Pane pane = new Pane();
+        
+        // Create the starting point (100, 100).
+        Line line = new Line(startX, startY, endX, endY);
+        
+        // Add starting point to the pane.
+        pane.getChildren().add(line);
+        
+        // Define an event handler for the different arrow keys.
+        line.setOnKeyPressed(e -> {
+            switch(e.getCode()) {
+                case RIGHT: // If user pressed right arrow key, draw line segment to the right.
+                    line.setStartX(line.getEndX());
+                    line.setStartY(line.getEndY());
+                    line.setEndX(line.getStartX() + 10);
+                    line.setEndY(line.getEndY());
+                    break;
+                case LEFT: // If user pressed left arrow key, draw line segment to the left.
+                    line.setStartX(line.getEndX());
+                    line.setStartY(line.getEndY());
+                    line.setEndX(line.getStartX() - 10);
+                    line.setEndY(line.getEndY());
+                    break;
+                case DOWN: // If user pressed down arrow key, draw line segment towards the bottom.
+                    line.setStartX(line.getEndX());
+                    line.setStartY(line.getStartY());
+                    line.setEndX(line.getEndX());
+                    line.setEndY(line.getEndY() + 10);
+                    break;
+                case UP: // If user pressed up arrow key, draw line segment towards the top.
+                    line.setStartX(line.getEndX());
+                    line.setStartY(line.getStartY());
+                    line.setEndX(line.getEndX());
+                    line.setEndY(line.getEndY() - 10);
+                    break;
+                default:       
+            }
+            
+            // Add the new line segment to the pane.
+            pane.getChildren().add(new Line(line.getStartX(), line.getStartY(), line.getEndX(), line.getEndY()));
+        });
+        
+        // Create a scene and add the pane.
+        Scene scene = new Scene(pane, 500, 300);
+        
+        // Display the scene in the stage.
+        primaryStage.setTitle("Exercise_15_09");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        
+        // Set the input focus on the line.
+        line.requestFocus();
+    }
+}
---
+++ IJPDS/Exercise_15_10/DisplayString.java
@@ -0,0 +1,56 @@
+/**
+ * Book: Introduction to Java Programming and Data Structures, 11th Edition (Y. Daniel Liang). 
+ * Exercise 15_10.
+ * (Enter and display a string) Write a program that receives a string from the
+ * keyboard and displays it on a pane. The Enter key signals the end of a string.
+ * Whenever a new string is entered, it is displayed on the pane.
+ *
+ * @author Almamy Bangoura
+ * @version 2/29/24
+ */
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.text.Text;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.input.KeyCode;
+import javafx.stage.Stage;
+
+public class DisplayString extends Application{
+    // Text variable to hold user input.
+    private Text text = new Text("");
+    
+    @Override
+    public void start(Stage primaryStage) {
+      // Create a pane for displaying the text.
+      StackPane pane = new StackPane();
+      pane.getChildren().add(text);
+      
+      // Create a textfield for the user to enter a string.
+      TextField textField = new TextField();
+      
+      // Create a border pane to hold both the pane and textfield.
+      BorderPane borderPane = new BorderPane();
+      borderPane.setCenter(pane);
+      borderPane.setBottom(textField);
+      
+      // Set up the keyboard 'Enter' key event handler.
+      textField.setOnKeyPressed(e -> {
+          if(e.getCode() == KeyCode.ENTER) { // If the user pressed the 'Enter' key...
+              text.setText(textField.getText()); // ... then update the text with user input.
+              textField.setText(""); // Set the textfield to an empty string.
+            }
+      });
+      
+      // Create a scene and place the borderpane in it.
+      Scene scene = new Scene(borderPane, 500, 300);
+      
+      // Place the scene in the stage and display it.
+      primaryStage.setTitle("Display a String");
+      primaryStage.setScene(scene);
+      primaryStage.show();
+    }
+}
